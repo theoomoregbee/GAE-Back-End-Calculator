6abcbfa260fa128a099814ff15dff70a31456440
Theophilus Omoregbee <theo4u@ymail.com>
2016-05-26 16:24:56 +0100

    done with commenting

--- calcBackend/src/main/java/com/calculator/calcBackend/CalcAPI.java
+++ calcBackend/src/main/java/com/calculator/calcBackend/CalcAPI.java
@@ -20,6 +20,12 @@ audiences = {Constants.ANDROID_AUDIENCE}
 
 public class CalcAPI {
 	
+	/**
+	 * Add method to add value1 and value2 
+	 * @param value1 method argument
+	 * @param value2 method argument
+	 * @return a bean object response to the requester
+	 */
 	@ApiMethod(name = "add")
 	public CalcBean add(@Named("value1") Integer value1, @Named("value2") Integer value2){
 		CalcBean response = new CalcBean();
@@ -27,7 +33,13 @@ public class CalcAPI {
 		return response;
 	}
 	
-	//Subtract value2 from value1
+	/**
+	 * 
+	 * Subtract method to subtract value2 from value 1
+	 * @param value1 The value property 
+	 * @param value2 The value2 property
+	 * @return
+	 */
 	public CalcBean subtract(@Named("value1") Integer value1, @Named("value2") Integer value2){
 		CalcBean response = new CalcBean();
 		response.setResult(String.valueOf((value1-value2)));
