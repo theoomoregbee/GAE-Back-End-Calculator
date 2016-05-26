3d3e755b826a10828d5423662df538fa4bf1babe
Theophilus Omoregbee <theo4u@ymail.com>
2016-05-26 12:54:25 +0100

    Setting up our calculator , sweet endpoint is going down in the next 45mins

---
+++ backend/calcBackend/src/main/java/com/calculator/calcBackend/YourFirstAPI.java
@@ -0,0 +1,8 @@
+package com.calculator.calcBackend;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+public class YourFirstAPI {
+}
